0c466833fabe5bae8a5d8c102bf26530ea6feb03
kirtanchandak <themarketingkirtan@gmail.com>
2022-12-20 20:56:05 +0530

    connection to contract and basic build done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
-import "./App.css";
+import "./App.scss";
+import { ethers } from "ethers";
+import abi from "./utils/WavePortal.json";
 
 const getEthereumObject = () => window.ethereum;
 
@@ -30,6 +32,8 @@ const findMetaMaskAccount = async () => {
 
 export default function App() {
   const [currentAccount, setCurrentAccount] = useState("");
+  const contractAddress = "0xaB7de5F67b50433B187c7A2897e0b1A190D0FD46";
+  const contractABI = abi.abi;
 
   const connectWallet = async () => {
     try {
@@ -47,6 +51,37 @@ export default function App() {
       console.error(error);
     }
   };
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        //Reading the contract -
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
     const account = findMetaMaskAccount();
     if (account !== null) {
@@ -57,14 +92,14 @@ export default function App() {
   return (
     <div className="mainContainer">
       <div className="dataContainer">
-        <div className="header">👋 Hey there!</div>
+        <div className="header">👋 Hey there, I am Kirtan!</div>
 
         <div className="bio">
-          I am Kirtan and I worked on self-driving cars so that's pretty cool
-          right? Connect your Ethereum wallet and wave at me!
+          I love to build things with Solidity🚀, pretty cool right? Connect
+          your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={connectWallet}>
+        <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
       </div>
